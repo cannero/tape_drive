968f75ab8ff837578a54ac4cf15543f7fe6ae773
cannero <serunmal@gmail.com>
2020-05-02 17:05:10 +0200

    build url for batch download

--- src/downloader.rs
+++ src/downloader.rs
@@ -12,17 +12,17 @@ const CLIENT_ID_KEY: &str = "TWITCH_CLIENT_ID";
 #[derive(Deserialize, Serialize)]
 struct UsersData {
     #[serde(rename = "data")]
-    users: Vec<TwitchUser>,
+    users: Vec<TwitchUserData>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct TwitchUser {
+pub struct TwitchUserData {
     id: String,
     display_name: String,
     view_count: u32,
 }
 
-pub async fn get_twitch_user(login: String) -> Result<TwitchUser, AsyncError> {
+pub async fn get_twitch_user(login: String) -> Result<TwitchUserData, AsyncError> {
     let client_id = match std::env::var(CLIENT_ID_KEY) {
         Ok(val) => val,
         Err(e) => panic!("twich client id not found, key {}, {}", CLIENT_ID_KEY, e),
@@ -34,4 +34,44 @@ pub async fn get_twitch_user(login: String) -> Result<TwitchUser, AsyncError> {
         .await?;
 
     Ok(users[0].clone())
+}
+
+fn build_urls(login_names: Vec<String>, chunk_size: usize) -> Vec<String> {
+    login_names
+        .chunks(chunk_size)
+        .map(|streamer_chunk| {
+            streamer_chunk
+                .iter()
+                .enumerate()
+                .map(|(i, login_name)| {
+                    let fmt = if i == 0 {
+                        "https://api.twitch.tv/helix/users?login="
+                    } else {
+                        "&login="
+                    };
+                    format!("{}{}", fmt, login_name)
+                })
+                .collect()
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_urls() {
+        let login_names = vec!["s_1".to_string(), "s_2".to_string(), "s_3".to_string()];
+
+        let urls = build_urls(login_names, 2);
+
+        assert_eq!(
+            urls,
+            vec![
+                "https://api.twitch.tv/helix/users?login=s_1&login=s_2",
+                "https://api.twitch.tv/helix/users?login=s_3"
+            ]
+        );
+    }
 }
